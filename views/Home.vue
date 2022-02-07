954252575b4727152b77602f3154f8125b587d38
tanjiaming99 <tanjiaming99@163.com>
2022-02-08 01:28:16 +0800

    feat:引入侧边栏
    aside

--- views/Home.vue
+++ views/Home.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="Home">我是Home页面</div>
+  <el-container style="height: 100%">
+    <el-aside width="auto">
+      <common-aside></common-aside>
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import CommonAside from "../src/components/CommonAside";
+
 export default {
   name: 'Home',
+  components:{
+    CommonAside
+  },
   data() {
     return {}
   }
 }
 </script>
+
+<style lang="less">
+.el-header{
+  background-color: #333;
+
+}
+.el-main{
+  padding-top: 0;
+}
+</style>
