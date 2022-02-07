d6af508f7a574e46248af5da4b6457b44e57b6a1
tanjiaming99 <tanjiaming99@163.com>
2022-02-08 01:45:43 +0800

    feat:无子项目的侧边栏
    模拟json数据
    使用了计算属性
    把值渲染上去

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -1,7 +1,11 @@
 <template>
   <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
            :collapse="isCollapse">
-    <el-submenu index="1">
+    <el-menu-item v-for="item in noChildren" :index="item.path" :key="item.path">
+      <i :class="'el-icon-'+item.icon"></i>
+      <span slot="title">{{ item.label }}</span>
+    </el-menu-item>
+    <el-submenu index="2">
       <template slot="title">
         <i class="el-icon-location"></i>
         <span slot="title">导航一</span>
@@ -9,28 +13,8 @@
       <el-menu-item-group>
         <span slot="title">分组一</span>
         <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
       </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-menu-item-group>
-      <el-submenu index="1-4">
-        <span slot="title">选项4</span>
-        <el-menu-item index="1-4-1">选项1</el-menu-item>
-      </el-submenu>
     </el-submenu>
-    <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
-      <span slot="title">导航二</span>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <i class="el-icon-document"></i>
-      <span slot="title">导航三</span>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <span slot="title">导航四</span>
-    </el-menu-item>
   </el-menu>
 </template>
 
@@ -45,7 +29,46 @@
 export default {
   data() {
     return {
-      isCollapse: true
+      isCollapse: false,
+      menu: [
+        {
+          path: '/',
+          name: 'home',
+          label: '首页',
+          icon: 's-home',
+          url: 'Home/Home'
+        }, {
+          path: '/mail',
+          name: 'mail',
+          label: '商品管理',
+          icon: 'video-play',
+          url: 'MailManage/MailManage'
+        }, {
+          path: '/user',
+          name: 'user',
+          label: '用户管理',
+          icon: 'user',
+          url: 'UserManage/UserManage'
+        }, {
+          label: '其它',
+          icon: 'location',
+          children: [
+            {
+              path: '/page1',
+              name: 'page1',
+              label: '页面1',
+              icon: 'setting',
+              url: 'Other/PageOne'
+            }, {
+              path: '/page2',
+              name: 'page2',
+              label: '页面2',
+              icon: 'setting',
+              url: 'Other/PageTwo'
+            }
+          ]
+        }
+      ]
     };
   },
   methods: {
@@ -54,6 +77,14 @@ export default {
     },
     handleClose(key, keyPath) {
       console.log(key, keyPath);
+    }
+  },
+  computed: {
+    noChildren() {
+      return this.menu.filter(item => !item.children)
+    },
+    hasChildren() {
+      return this.menu.filter(item => item.children)
     }
   }
 }
