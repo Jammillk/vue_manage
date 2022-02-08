b53cbb6ca76830268c1b669c01a2baccd8fcba3f
tanjiaming99 <tanjiaming99@163.com>
2022-02-08 13:23:53 +0800

    feat:header样式完成

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -2,18 +2,19 @@
   <header>
     <div class="l-content">
       <el-button plain icon="el-icon-menu" size="mini"></el-button>
-      <h3 style="color: #fff"></h3>
-      <div class="r-content">
-        <el-dropdown trigger="click" size="mini">
-            <span>
-              <img class="user" :src="userImg">
-            </span>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>个人中心</el-dropdown-item>
-            <el-dropdown-item>退出</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-      </div>
+      <h3 style="color: #fff">首页</h3>
+    </div>
+    <div class="r-content">
+      <el-dropdown trigger="click" size="mini">
+          <span>
+            <img class="user" :src="userImg">
+          </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>个人中心</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+
     </div>
   </header>
 </template>
@@ -28,3 +29,30 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+header {
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.l-content {
+  display: flex;
+  align-items: center;
+
+  .el-button {
+    margin-right: 20px;
+  }
+}
+
+.r-content {
+  .user {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+}
+
+</style>
