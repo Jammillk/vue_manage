3d0ca4c61683c8ed9e278a9a597c3ff0dfd4fb19
tanjiaming99 <tanjiaming99@163.com>
2022-02-08 14:49:34 +0800

    feat:表单渲染数据

--- views/home/index.vue
+++ views/home/index.vue
@@ -1,12 +1,85 @@
 <template>
-  <div >我是home页面</div>
+  <el-row class="home" :gutter="20">
+    <el-col :span="8" style="margin-top: 20px">
+      <el-card shadow="hover">
+        <div class="user">
+          <img :src="userImg">
+          <div class="userinfo">
+            <p class="name">Admin</p>
+            <p class="access">超级管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登录时间：<span>2022-01-01</span></p>
+          <p>上次登录地点：<span>广州</span></p>
+        </div>
+      </el-card>
+      <el-card style="margin-top: 20px; height: 460px">
+        <el-table :data="tableData">
+<!--prop对应对象中的键名   key是name、todayBuy这些       -->
+          <el-table-column
+              v-for="(val, key) in tableLabel"
+              :key="key"
+              :prop="key"
+              :label="val">
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
   name: 'Home',
   data() {
-    return {}
+    return {
+      userImg: require('../../src/assets/images/user.png'),
+      tableData: [
+        {
+          name: 'oppo',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'vivo',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '苹果',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '小米',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '三星',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '魅族',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        }
+      ],
+      tableLabel: {
+        name: '课程',
+        todayBuy: '今日购买',
+        monthBuy: '本月购买',
+        totalBuy: '总购买'
+      }
+    }
   }
 }
 </script>
