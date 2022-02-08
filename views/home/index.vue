da86964762aea0f1f2e8e04f204f56e25e035d43
tanjiaming99 <tanjiaming99@163.com>
2022-02-09 00:19:11 +0800

    feat:饼图完成
    好像。。跟着改改数据就行了 ？？

--- views/home/index.vue
+++ views/home/index.vue
@@ -51,7 +51,9 @@
         <el-card style="height: 265px">
           <div style="height: 240px" ref="userEcharts"></div>
         </el-card>
-        <el-card style="height: 265px"></el-card>
+        <el-card style="height: 265px">
+          <div style="height: 240px" ref="videoEcharts"></div>
+        </el-card>
       </div>
     </el-col>
   </el-row>
@@ -202,6 +204,29 @@ export default {
         }
         const U = echarts.init(this.$refs.userEcharts)
         U.setOption(userOption)
+        // 饼图
+        const videoOption = {
+          tooltip: {
+            trigger: "item",
+          },
+          color: [
+            "#0f78f4",
+            "#dd536b",
+            "#9462e5",
+            "#a6a6a6",
+            "#e1bb22",
+            "#39c362",
+            "#3ed1cf",
+          ],
+          series: [
+            {
+              data: data.videoData,
+              type: 'pie'
+            }
+          ],
+        }
+        const V = echarts.init(this.$refs.videoEcharts)
+        V.setOption(videoOption)
       }
 
       console.log(res)
