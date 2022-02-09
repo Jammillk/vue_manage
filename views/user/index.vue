7addf899ee0d99c4b8ee18d47dbce16016357915
tanjiaming99 <tanjiaming99@163.com>
2022-02-09 21:35:40 +0800

    feat：使用封装form组件，完成新增、弹窗、搜索框

--- views/user/index.vue
+++ views/user/index.vue
@@ -1,12 +1,118 @@
 <template>
-  <div class="User">我是User页面</div>
+  <div class="manage">
+    <el-dialog
+        :title="operateType === 'add' ? '新增用户': '更新用户'"
+        :visible.sync="isShow">
+      <common-form
+          :form-label="operateFormLabel"
+          :form="operateForm"
+          :inline="true"
+          ref="form"
+      ></common-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="isShow = false">取消</el-button>
+        <el-button type="primary" @click="confirm">确定</el-button>
+      </div>
+    </el-dialog>
+    <div class="manage-header">
+      <el-button type="primary" @click="addUser">新增</el-button>
+      <common-form
+          :form-label="formLabel"
+          :form="searchForm"
+          :inline="true"
+          ref="form">
+        <el-button type="primary" @click="getList">搜索</el-button>
+      </common-form>
+    </div>
+  </div>
 </template>
 
 <script>
+import CommonForm from "../../src/components/CommonForm";
+
 export default {
   name: 'User',
+  components: {
+    CommonForm
+  },
   data() {
-    return {}
+    return {
+      operateType: 'add',
+      isShow: false,
+      operateFormLabel: [
+        {
+          model: 'name',
+          label: '姓名',
+          type: 'input'
+        },
+        {
+          model: 'age',
+          label: '年龄',
+          type: 'input'
+        },
+        {
+          model: 'sex',
+          label: '性别',
+          type: 'select',
+          opts: [
+            {
+              label: '男',
+              value: 1
+            },
+            {
+              label: '女',
+              value: 0
+            }
+          ]
+        },
+        {
+          model: 'birth',
+          label: '出生日期',
+          type: 'date'
+        },
+        {
+          model: 'addr',
+          label: '地址',
+          type: 'input'
+        }
+      ],
+      operateForm: {
+        name: '',
+        addr: '',
+        age: '',
+        brith: '',
+        sex: ''
+      },
+      formLabel: [
+        {
+          model: 'keyword',
+          label: '',
+          type: 'input'
+        }
+      ],
+      searchForm: {
+        keyword: ''
+      }
+    }
+  },
+  methods: {
+    confirm() {
+
+    },
+    addUser() {
+      this.isShow = true
+      this.operateType = 'add'
+      this.operateForm = {
+        name: '',
+        addr: '',
+        age: '',
+        brith: '',
+        sex: ''
+      }
+    },
+    getList() {
+
+    }
   }
 }
 </script>
